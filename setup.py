f597e7d16c4feb55bf96fd78354492f254004207
Peter Smit <peter@cis.hut.fi>
2011-06-09 15:56:00 +0300

    Fixed bug in setup.py and changed version to 0.0.1dev

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(name='smit.launchserver',
-      version='alpha0',
+      version='0.0.1dev',
       description='Tool for launching an amazon web server',
       author='Peter Smit',
       author_email='peter@smitmail.eu',
-      packages=['smit.launchserver'],
+      packages=['smit'],
       package_dir={'': 'src'},
       install_requires=['boto'],
       entry_points = dict(console_scripts=[
